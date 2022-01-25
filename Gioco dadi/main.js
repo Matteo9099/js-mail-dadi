ee6f630f1abd2bd3ea0fb078001e79a5337e6525
Matteo Morganti <matteo99morganti@gmail.com>
2022-01-25 17:57:56 +0100

    push

--- Gioco dadi/main.js
+++ Gioco dadi/main.js
@@ -6,18 +6,22 @@ game.addEventListener('click',
         let player1 = document.getElementById('player1').value;
         let player2 = document.getElementById('player2').value;
 
-        document.getElementById('user').innerHTML = "Numero " + player1 + ": "  + Math.floor(Math.random()*6);
-        document.getElementById('computer').innerHTML = "Numero "+ player2 + ": " + Math.floor(Math.random()*6);
+       const dado1 = Math.floor(Math.random()* 6) +1;
+       const dado2 = Math.floor(Math.random()* 6) +1; 
 
-     if(player1 > player2){
+       document.getElementById('user').innerHTML= "Numero " + player1 +": " + dado1;
+       document.getElementById('computer').innerHTML= "Numero " + player2 +": " + dado2;
+
+     if(dado1 > dado2){
          document.getElementById('result').innerHTML= "ha vinto " + player1;
      }
-     else if(player1 == player2){
-        document.getElementById('result').innerHTML= "Pareggio";
-     }
+      else if(dado1 < dado2){
+         document.getElementById('result').innerHTML= "Ha vinto " + player2;
+      }
      else{
-        document.getElementById('result').innerHTML= "ha vinto " + player2;
-     }
+        document.getElementById('result').innerHTML= "Pareggio";
+
+       }
 
     }
 )
