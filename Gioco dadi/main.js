354fd313fed5e2fbff7f12b29c8825c63807a6b8
Matteo Morganti <matteo99morganti@gmail.com>
2022-01-25 23:10:11 +0100

    modifiche estetiche

--- Gioco dadi/main.js
+++ Gioco dadi/main.js
@@ -9,8 +9,8 @@ game.addEventListener('click',
        const dado1 = Math.floor(Math.random()* 6) +1;
        const dado2 = Math.floor(Math.random()* 6) +1; 
 
-       document.getElementById('user').innerHTML= "Numero " + player1 +": " + dado1;
-       document.getElementById('computer').innerHTML= "Numero " + player2 +": " + dado2;
+       document.getElementById('user').innerHTML= player1 +" lancia il dado... è uscito il numero:  " + dado1;
+       document.getElementById('computer').innerHTML= player2 +" lancia il dado... è uscito il numero:  " + dado2;
 
      if(dado1 > dado2){
          document.getElementById('result').innerHTML= "ha vinto " + player1;
